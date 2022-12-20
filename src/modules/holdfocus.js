7c9e1f308a5895aec61f293328b5560c82c62b2c
nazarisabbot <n.updesign@gmail.com>
2022-12-21 01:36:38 +0600

    Form Work 2

--- src/modules/holdfocus.js
+++ src/modules/holdfocus.js
@@ -1,2 +1,12 @@
-label = document.querySelector("label");
-console.log(label);
+const input = document.querySelectorAll("[data-labal-marker]");
+const inputArr = Array.from(input);
+
+inputArr.forEach((el) => {
+  el.onblur = function (event) {
+    if (event.target.value) {
+      el.classList.add("value-exist");
+    } else {
+      el.classList.remove("value-exist");
+    }
+  };
+});
